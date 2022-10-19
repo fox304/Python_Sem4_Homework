e2f7e7b1a8604eaac2b8dd1b6655e5368b76fed1
Maks <nordmax69@gmail.com>
2022-10-19 19:14:20 +0700

    added task 2

---
+++ home_2.py
@@ -0,0 +1,17 @@
+# Задайте натуральное число N. Напишите программу,
+#  которая составит список простых множителей числа N.
+num = int(input("Введите число : "))
+list_1 = []
+
+
+def simple_mult(a):
+    for i in range(2,a + 1):
+        if a == i:
+            list_1.append(i)
+            return 
+        if a % i == 0:
+            list_1.append(i)
+            return simple_mult(a//i)
+    return list_1
+simple_mult(num)
+print(f"Список простых множителей числa {num} равен ",list_1)
